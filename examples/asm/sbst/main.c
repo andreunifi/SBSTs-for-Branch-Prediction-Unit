0b8b78c91b9e972432b057f39f89a30752276436
andreunifi <s338780@po.polito.it>
2026-03-06 16:54:39 +0100

    finaized bpu

--- examples/asm/sbst/main.c
+++ examples/asm/sbst/main.c
@@ -74,33 +74,74 @@ int main(void)
 
 	register uint32_t dbg3 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
 
-	i=7;
-
-
-	hpm_before = compute_signature();
-
-	test7();
-
-	hpm_after = compute_signature();
-
-	ACTUAL_SIGNATURES[i] = hpm_after - hpm_before; //store the difference in the second element
-
-	register uint32_t dbg7 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
-
 	i++;
 
 
-	hpm_before = compute_signature();
+	// hpm_before = compute_signature();
 
-	test8();
+	// test4();
 
-	hpm_after = compute_signature();
+	// hpm_after = compute_signature();
 
-	ACTUAL_SIGNATURES[i] = hpm_after - hpm_before; //store the difference in the second element
+	// ACTUAL_SIGNATURES[i] = hpm_after - hpm_before; //store the difference in the second element
 
-	register uint32_t dbg8 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
+	// register uint32_t dbg4 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
 
-	i++;
+
+	// i++;
+
+	// hpm_before = compute_signature();
+
+	// test5();
+
+	// hpm_after = compute_signature();
+
+	// ACTUAL_SIGNATURES[i] = hpm_after - hpm_before; //store the difference in the second element
+
+	// register uint32_t dbg5 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
+
+	// i++;
+
+	// hpm_before = compute_signature();
+
+	// test6();
+
+	// hpm_after = compute_signature();
+
+	// ACTUAL_SIGNATURES[i] = hpm_after - hpm_before; //store the difference in the second element
+
+	// register uint32_t dbg6 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
+
+	// i++;
+
+
+
+
+	
+	// hpm_before = compute_signature();
+
+	// test7();
+
+	// hpm_after = compute_signature();
+
+	// ACTUAL_SIGNATURES[i] = hpm_after - hpm_before; //store the difference in the second element
+
+	// register uint32_t dbg7 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
+
+	// i++;
+
+
+	// hpm_before = compute_signature();
+
+	// test8();
+
+	// hpm_after = compute_signature();
+
+	// ACTUAL_SIGNATURES[i] = hpm_after - hpm_before; //store the difference in the second element
+
+	// register uint32_t dbg8 asm("t0") = ACTUAL_SIGNATURES[i]; //check it manually 
+
+	// i++;
 
 
 
